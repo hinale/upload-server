4413dab1d8f14ab1acfac531d679f6ef28f87f2a
Letícia Amaral <leticiaamarallara@hotmail.com>
2025-02-01 16:50:16 -0300

    feat: configuração zod e swagger

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,10 +1,56 @@
-import { fastify } from "fastify";
-import { fastifyCors } from "@fastify/cors";
+import { env } from '@/env'
+import { fastifyCors } from '@fastify/cors'
+import { fastify } from 'fastify'
+import {
+  hasZodFastifySchemaValidationErrors,
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import { uploadImageRoute } from './roots/upload-image'
+import { fastifyMultipart } from '@fastify/multipart'
+import { fastifySwagger } from '@fastify/swagger'
+import { fastifySwaggerUi } from '@fastify/swagger-ui'
 
-const server = fastify();
+const server = fastify()
 
-server.register(fastifyCors, { origin: "*"});
+server.setValidatorCompiler(validatorCompiler)
+server.setSerializerCompiler(serializerCompiler)
 
-server.listen({port: 3333, host: "0.0.0.0"}).then(() => {
-  console.log("HTTP Server is running on port 3333");
-});
+server.setErrorHandler((error, request, reply) => {
+  if (hasZodFastifySchemaValidationErrors(error)) {
+    return reply.status(400).send({
+      message: 'Validation Error',
+      issues: error.validation,
+    })
+  }
+
+  // Envia o erro p/ alguma ferramenta de observabilidade (Sentry, Datadog, Grafana, OpenTelemetry, etc)
+
+  console.error(error)
+
+  return reply.status(500).send({ message: 'Internal Server Error' })
+})
+
+server.register(fastifyCors, { origin: '*' })
+
+server.register(fastifyMultipart)
+server.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "Upload Server",
+      version: "1.0.0"
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+server.register(fastifySwaggerUi, {
+  routePrefix: '/docs'
+})
+
+server.register(uploadImageRoute)
+
+server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
+  console.log('HTTP Server is running on port 3333')
+})
